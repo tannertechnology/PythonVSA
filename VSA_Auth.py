3970724850b3ab517c6526eab7de329a5f00f8a7
Tanner Anderson <me@tanner.technology>
2020-07-11 02:01:40 -0600

    Finish Email OAuth flow

--- VSA_Auth.py
+++ VSA_Auth.py
@@ -7,6 +7,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from time import sleep
 import re
 from imaplib import IMAP4_SSL
 import email
@@ -29,7 +30,6 @@ try:
     redirect_uri = config['Listener']['redirect_uri']
     listen_port = config['Listener']['listen_port']
 
-    #email_uri = config['Email'].getboolean('email_uri')
     smtp_username = config['Email']['smtp_username']
     smtp_password = config['Email']['smtp_password']
     smtp_emailfrom = config['Email']['smtp_emailfrom']
@@ -42,6 +42,7 @@ try:
     imap_email = config['Email']['imap_email']
     imap_server = config['Email']['imap_server']
     imap_port = int(config['Email']['imap_port'])
+    imap_refresh_interval = int(config['Email']['imap_refresh_interval'])
 
 except(KeyError):
     print("A required variable is missing. RIPERONI. ")
@@ -52,11 +53,7 @@ authendpoint = vsa_uri + "/api/v1.0/authorize"
 urlforuser = vsa_uri + "/vsapres/web20/core/login.aspx?response_type=code&redirect_uri=" + redirect_uri + "&client_id=" + client_id
 
 
-def doInitialAuth(body):
-    print("ImportListener found someone authing!")
-    # Get first refresh token
-    #code = body.split("?code=")[1]
-    code = body
+def doInitialAuth(code):
     r = requests.post(authendpoint, json={
         "grant_type": "authorization_code",
         "code": code,
@@ -79,20 +76,22 @@ def doInitialAuth(body):
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
     VSA.Auth.doRefresh(refreshtoken)
-    
+
+
 if __name__ == "__main__":
     print("Attempting initial auth")
     print("Please visit this link and copy the entire resulting URL.")
     print(urlforuser)
 
     msg = MIMEMultipart('mixed')
-    msg['Subject'] = "VSAPY Authentication"
+    msg['Subject'] = "PythonVSA Authentication"
     msg['From'] = smtp_emailfrom
     msg['To'] = smtp_emailto
-    message = MIMEText('Please follow this link to authenticate your new integration: ' + urlforuser)
-    message1 = MIMEText('Reply to this message with the full URL that is returned to complete authentication.')
-    msg.attach(message)
+    text = f"""\
+        Please follow this link to authenticate your new integration:  {urlforuser}
 
+        Once you have authorized you will be redirected to a page that doesn't load/resolve. Copy the address from your address bar and reply to this email with it."""
+    msg.attach(MIMEText(text))
 
     smtp_server = smtplib.SMTP(smtp_server, smtp_port)
     smtp_server.ehlo()
@@ -102,18 +101,53 @@ if __name__ == "__main__":
     smtp_server.sendmail(smtp_emailfrom, smtp_emailto, msg.as_string())
     smtp_server.close()
 
+    print(f"Waiting {imap_refresh_interval} seconds to give a chance to respond.")
+    sleep(imap_refresh_interval)
+
     connection = IMAP4_SSL(imap_server, imap_port)
     connection.login(imap_username, imap_password)
     typ, data = connection.select('INBOX')
-    typ, data = connection.search(None, '(UNSEEN SUBJECT "%s")' % "VSAPY Authentication")
+    typ, data = connection.search(None, '(UNSEEN)')
+
+    if(data == [b'']):
+        print(f"No emails found. Checking again every {imap_refresh_interval} seconds.")
+        i = 0
+        while i < 5:
+            i = i + 1
+            print(f"Checking {5 - i} more times.")
+            sleep(imap_refresh_interval)
+            typ, data = connection.search(None, '(UNSEEN)')
+            if(data[0] != b''):
+                break
+
     for num in data[0].split():
         typ, data = connection.fetch(num, '(RFC822)')
-        msg = email.message_from_bytes(data[0][1])
-        typ, data = connection.store(num, '-FLAGS','\\Seen')
-        print(msg)
-        # Parse message here, if we don't find any emails with a code try again
+        try:
+            msg = email.message_from_bytes(data[1][1])
+        except(IndexError):
+            msg = email.message_from_bytes(data[0][1])
 
-    #code = input("Paste code here: ")
-    #TODO: Monitor mailbox for response here
-    #doInitialAuth(code)
-    pass
+        typ, data = connection.store(num, '+FLAGS', '\\Seen')
+        print(msg._payload)
+        pattern = r'https://.*\/\?code(=[\w\d]{34})'
+        pattern1 = r'https://.*\/\?code(=[\w\d]{32})'
+        try:
+            match = re.match(pattern, msg._payload)
+            matchraw = re.match(pattern1, msg._payload)
+        except(TypeError):
+            print("It appears we have a message with a format we can't understand. Deleting.")
+            continue
+        if(match):
+            code = match.group(1)
+            code = code.replace("=3D", "")
+            connection.close()
+            connection.logout()
+            doInitialAuth(code)
+        elif(matchraw):
+            connection.close()
+            connection.logout()
+            doInitialAuth(code)
+        else:
+            print("Didn't find URL. Deleting.")
+            continue
+    print("All done.")
