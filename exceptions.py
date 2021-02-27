6605eae617e83476b3e3d4120ac828e1fe10796c
Tanner Anderson <me@tanner.technology>
2021-02-26 20:32:11 -0700

    Adjust logging of execptions

--- exceptions.py
+++ exceptions.py
@@ -2,19 +2,19 @@ class VSAError(Exception):
     """Generic error returned from VSA"""
     def __init__(self, *args):
         import inspect
-        print("------------------ Begin details ------------------")
+        print("------------------ Begin VSAError ------------------")
         print(f"A generic error occurred in {inspect.stack()[1].function}.")
         print("If more details are available they will be printed below:")
         if args:
             print(f"{args[0]}")
-        print("------------------ End details ------------------")
+        print("------------------ End VSAError ------------------")
 
 
 class ItemNotFound(VSAError):
-    """We were either unable to find the requested ticket or status."""
+    """We were unable to find the requested ticket or status."""
     def __init__(self, RequestObject):
-        print("------------------ Begin details ------------------")
+        print("------------------ Begin ItemNotFound ------------------")
         print("An ID/Reference in the request was not found in Kaseya.")
         print(f"We tried to contact this url: {RequestObject.url}")
         print(f"Raw response from Kaseya:\n {RequestObject.text}")
-        print("------------------ End details ------------------")
+        print("------------------ End ItemNotFound ------------------")
